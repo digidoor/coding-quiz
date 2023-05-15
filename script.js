f369481be4f9281b1668f804433c6aaa600920c8
digidoor <37601466+digidoor@users.noreply.github.com>
2023-05-15 09:29:34 -0700

    added sound and js files

---
+++ script.js
@@ -0,0 +1,15 @@
+var startButton = document.getElementById('startButton');
+var enterScore = document.getElementById('initials');
+
+function playGame()
+{
+	var stuff;
+}
+
+function updateScoreboard()
+{
+	var stuff;
+}
+
+startButton.addEventListener('click', playGame);
+enterScore.addEventListener('submit', updateScoreboard);

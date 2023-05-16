398d658a7993cf96b5e193cace03def23ed69fb8
digidoor <37601466+digidoor@users.noreply.github.com>
2023-05-15 18:36:10 -0700

    added .css, quiz now shows

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
 var startButton = document.getElementById('startButton');
-var enterScore = document.getElementById('result');
+var initialsForm = document.getElementById('result');
+var questionsEl = document.getElementById('questions');
 
 const audioRight = new Audio('mixkit-correct-answer-tone-2870.wav');
 const audioWrong = new Audio('mixkit-wrong-electricity-buzz-955.wav');
 
+var questions =
+[
+	{
+		question: 'Commonly used data types DO NOT include:',
+		answers: ['strings', 'booleans', 'alerts', 'numbers'],
+		correct: 'alerts'
+	},
+	{
+		question: 'The condition in an if / else statement is enclosed within ____.',
+		answers: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+		correct: 'parentheses'
+	},
+	{
+		question: 'Arrays in JavaScript can be used to store ____.',
+		answers: [ 'numbers and strings', 'other arrays', 'booleans', 'all of the above' ],
+		correct: 'all of the above'
+	},
+	{
+		question: 'String values must be enclosed within ____ when being assigned to variables.',
+		answers: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+		correct: 'quotes'
+	},
+	{
+		question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+		answers: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+		correct: 'console.log'
+	}
+];
+
 function playGame()
 {
 	audioWrong.play();
+	for( entry of questions)
+	{
+		var title = document.createElement("h3");
+		title.textContent = entry.question;
+		questionsEl.append(title);
+		for(i=0; i<entry.answers.length; i++)
+		{
+			var answer = document.createElement("button");
+			answer.textContent = entry.answers[i];
+			questionsEl.append(answer);
+		}
+	}
 }
 
 function updateScoreboard()
@@ -16,4 +58,4 @@ function updateScoreboard()
 }
 
 startButton.addEventListener('click', playGame);
-enterScore.addEventListener('submit', updateScoreboard);
+initialsForm.addEventListener('submit', updateScoreboard);
